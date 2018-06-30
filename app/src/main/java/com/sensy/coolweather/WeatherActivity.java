edf823435a80a0f0abc9e6ddd6a838f7e9118832
SensyJiang <sensyjiang@gmail.com>
2018-06-30 18:03:07 +0800

    增加后台自动更新天气功能。

--- app/src/main/java/com/sensy/coolweather/WeatherActivity.java
+++ app/src/main/java/com/sensy/coolweather/WeatherActivity.java
@@ -22,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.sensy.coolweather.gson.Forecast;
 import com.sensy.coolweather.gson.Weather;
+import com.sensy.coolweather.service.AutoUpdateService;
 import com.sensy.coolweather.util.HttpUtil;
 import com.sensy.coolweather.util.Utility;
 
@@ -230,6 +231,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
 }
